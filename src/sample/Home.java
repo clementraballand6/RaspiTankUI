ca6aa4ed161401cb5ab77fc22c46c0cd9e05b40f
clément <clement.raballand6@gmail.com>
2016-12-13 17:08:38 +0100

    Mise en place keyPress events sur arrow keys

--- src/sample/Home.java
+++ src/sample/Home.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 
@@ -18,12 +19,13 @@ public class Home implements Initializable {
     public Button left;
     public Button planPath;
     public Button showPaths;
+    public String lastKey = "";
 
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
         this.planPath.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
-                if(mouseEvent.getButton().equals(MouseButton.PRIMARY)){
+                if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
                     try {
                         Main.go("path");
                     } catch (IOException e) {
@@ -36,7 +38,7 @@ public class Home implements Initializable {
         this.showPaths.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
-                if(mouseEvent.getButton().equals(MouseButton.PRIMARY)){
+                if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
                     try {
                         Main.go("viewPath");
                     } catch (IOException e) {
@@ -45,5 +47,34 @@ public class Home implements Initializable {
                 }
             }
         });
+    }
+
+    public void keyPressed(KeyEvent keyEvent) throws Exception {
+        String keyName = keyEvent.getCode().getName();
+        if (!this.lastKey.equals(keyName)) {
+            if ("Right".equals(keyName)) {
+                System.out.println(keyName);
+                Http.get("direction/right");
+            }
+            if ("Left".equals(keyName)) {
+                Http.get("direction/left");
+                System.out.println(keyName);
+            }
+            if ("Up".equals(keyName)) {
+                Http.get("direction/forward");
+                System.out.println(keyName);
+            }
+            if ("Down".equals(keyName)) {
+                Http.get("direction/backward");
+                System.out.println(keyName);
+            }
+        }
+
+        this.lastKey = keyName;
+
+    }
+
+    public void keyReleased(KeyEvent keyEvent) {
+        this.lastKey = "";
     }
 }
